2b7da856bc9c7bab2f7207017c60669f43c14699
Joao-Girotto <joaogirotto34@gmail.com>
2024-12-03 15:13:11 -0300

    contributing: code testing

--- Strategy/src/Main.java
+++ Strategy/src/Main.java
@@ -2,15 +2,15 @@ import strategy.*;
 
 public class Main {
     public static void main(String[] args) {
-        CalculadoraDePreco calculadora;
+        CalculadoraPreco calculadora;
 
-        calculadora = new CalculadoraDePreco(new SemDesconto());
+        calculadora = new CalculadoraPreco(new SemDesconto());
         System.out.println("Preço final (Sem Desconto): " + calculadora.calcularPrecoFinal(100));
 
-        calculadora = new CalculadoraDePreco(new DescontoPercentual(10));
+        calculadora = new CalculadoraPreco(new DescontoPercentual(10));
         System.out.println("Preço final (Desconto Percentual): " + calculadora.calcularPrecoFinal(100));
 
-        calculadora = new CalculadoraDePreco(new DescontoFixo(15));
+        calculadora = new CalculadoraPreco(new DescontoFixo(15));
         System.out.println("Preço final (Desconto Fixo): " + calculadora.calcularPrecoFinal(100));
     }
 }
